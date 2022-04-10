52a6cae9f2ad0aa8599d4f2a2a92e4321b9ef74a
gregkillinwtf <ayomidegreg@gmail.com>
2022-04-10 09:37:44 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (success/correct)
- *
- *     */
-
+ * Return: Always 0 (success/correct)
+ */
 int main(void)
 
 {
