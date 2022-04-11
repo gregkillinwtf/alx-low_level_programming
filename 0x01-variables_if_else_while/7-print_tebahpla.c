148aa9c32ecabf026bbfb618369153748bb9b1dd
gregkillinwtf <ayomidegreg@gmail.com>
2022-04-11 07:04:09 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ *
+ */
+
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 122 ; alphabet >= 97; alphabet--)
+{
+putchar(alphabet);
+}
+
+putchart('\n');
+
+return (0);
+
+
+}
