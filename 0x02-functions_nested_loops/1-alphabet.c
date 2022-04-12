ed353766b0fdbc66fe2e2b813013edffeba45ab8
gregkillinwtf <ayomidegreg@gmail.com>
2022-04-12 10:19:33 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,27 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print alphabet
+ * print_alphabet - print alphabet lowercase
  *
  * Return: Always 0.
  */
-void print_alphabet_x10(void)
-{
-int alphabet;
-int count;
 
-
-count = 0;
-while (count < 10)
+void print_alphabet(void)
 {
-for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 _putchar(alphabet);
 }
 
-count++;
 _putchar('\n');
-}
-
 
 }
