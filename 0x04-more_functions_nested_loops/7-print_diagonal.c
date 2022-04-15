6b68f49577683e7215ef89a294d0d7755ad42b70
gregkillinwtf <ayomidegreg@gmail.com>
2022-04-15 14:23:14 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,7 +13,7 @@ int i = 0, ii;
 
 while (i < n && n > 0)
 {
-ii = 0,
+ii = 0;
 while (ii < i)
 {
 _putchar(' ');
@@ -21,7 +21,7 @@ ii++;
 }
 
 _putchar('\\');
-_putchar('\n');i++;
+_putchar('\n');
 i++;
 }
 if (i == 0)
